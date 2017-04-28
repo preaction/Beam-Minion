2aa4049464f0062ec9750a7c9e7812e067957957
Doug Bell <doug@preaction.me>
2017-04-28 01:17:04 -0500

    add some initial getting started documentation

--- lib/Beam/Minion.pm
+++ lib/Beam/Minion.pm
@@ -4,6 +4,7 @@ our $VERSION = '0.003';
 
 =head1 SYNOPSIS
 
+    export BEAM_MINION=sqlite://test.db
     beam minion worker <container>...
     beam minion run <container> <service> [<args>...]
     beam minion help
@@ -12,7 +13,68 @@ our $VERSION = '0.003';
 
 L<Beam::Minion> is a distributed task runner. One or more workers are
 created to run tasks, and then each task is sent to a worker to be run.
-Tasks are configured by L<Beam::Wire> container files.
+Tasks are configured as L<Beam::Runnable> objects by L<Beam::Wire>
+container files.
+
+=head1 GETTING STARTED
+
+=head2 Configure Minion
+
+To start running your L<Beam::Runner> jobs, you must first start
+a L<Minion> worker with the L<beam minion
+worker.command|Beam::Minion::Command::worker>.  Minion requires
+a database to coordinate workers, and communicates with this database
+using a L<Minion::Backend>.
+
+The supported Minion backends are:
+
+=over
+
+=item *
+
+L<Minion::Backend::SQLite> - C<< sqlite:<db_path> >>
+
+=item *
+
+L<Minion::Backend::Pg> - C<< postgresql://<user>:<pass>@<host>/<database> >>
+
+=item *
+
+L<Minion::Backend::mysql> - C<< mysql://<user>:<pass>@<host>/<database> >>
+
+=item *
+
+L<Minion::Backend::MongoDB> - C<< mongodb://<host>:<port> >>
+
+=back
+
+Once you've picked a database backend, configure the C<BEAM_MINION>
+environment variable with the URL. Minion will automatically deploy the
+database tables it needs, so be sure to allow the right permissions (if
+the database has such things).
+
+In order to communicate with Minion workers on other machines, it will
+be necessary to use a database accessible from the network (so, not
+SQLite).
+
+=head2 Start a Worker
+
+Once the C<BEAM_MINION> environment variable is set, you can start
+a worker with C<< beam minion worker <container> >>. Each worker can run
+jobs from one container, specified as the argument to the C<beam minion
+worker> command. Each worker will run up to 4 jobs concurrently.
+
+=head2 Spawn a Job
+
+Jobs are spawned with C<< beam minion run <container> <service> >>.
+The C<service> must be an object that consumes the L<Beam::Runnable>
+role. C<container> should be a path to a container file and can be
+an absolute path, a path relative to the current directory, or a
+path relative to one of the paths in the C<BEAM_PATH> environment
+variable (separated by C<:>).
+
+You can queue up jobs before you have workers running. As soon as
+a worker is available, it will start running jobs from the queue.
 
 =head1 SEE ALSO
 
--- lib/Beam/Minion/Command/run.pm
+++ lib/Beam/Minion/Command/run.pm
@@ -14,6 +14,35 @@ C<service> from the given C<container>.
 In order for the job to run, you must run a Minion worker using the
 L<beam minion worker command|Beam::Minion::Command::worker>.
 
+=head1 ARGUMENTS
+
+=head2 container
+
+The container that contains the task to run. This can be an absolute
+path to a container file, a relative path from the current directory, or
+a relative path from one of the directories in the C<BEAM_PATH> environment
+variable (separated by C<:>).
+
+=head2 service
+
+The service that defines the task to run. Must be an object that consumes
+the L<Beam::Runner> role.
+
+=head1 ENVIRONMENT
+
+=head2 BEAM_MINION
+
+This variable defines the shared database to coordinate the Minion workers. This
+database is used to queue the job. This must be the same for all workers
+and every job running.
+
+See L<Beam::Minion/Getting Started> for how to set this variable.
+
+=head2 BEAM_PATH
+
+This variable is a colon-separated list of directories to search for
+containers.
+
 =head1 SEE ALSO
 
 L<Beam::Minion>, L<Minion>
